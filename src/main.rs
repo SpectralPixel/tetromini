94d359d553224e50e837583382302f38a15bce8e
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-20 19:36:26 +0200

    Changed map size

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bevy::{prelude::*, window::{WindowMode, WindowResolution}};
 use bevy_pixel_buffer::prelude::*;
 
 const MAP_DIMS: PixelBufferSize = PixelBufferSize {
-    size: UVec2::new(15, 15),
+    size: UVec2::new(10, 20),
     pixel_size: UVec2::new(20, 20), 
 };
 
