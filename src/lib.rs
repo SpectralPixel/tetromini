ab9a8c1ff7fbfc7f0f31bdaeb61d4b6ce675d4b7
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-20 20:52:38 +0200

    Added test to check if default is being derived correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
 #[derive(Default)]
 pub struct GameMap([u16; 15]);
+
+#[cfg(test)]
+mod tests {
+    use crate::GameMap;
+
+    #[test]
+    fn create_empty_map() {
+        let map = GameMap::default();
+        for num in map.0 {
+            assert_eq!(num, 0);
+        }
+    }
+}
